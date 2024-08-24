249415b0b6772824ade244b8d32f6ef0da513052
Ben McCarthy <bmccarthy90@gmail.com>
2024-08-24 12:33:52 -0400

    tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
+import ipdb
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        
+        if i >= 1:
+            print (i)
+        elif i == 0:
+            print ("Happy New Year!")
+        i = i - 1
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int * int for int in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   i = 1
+   while (i <= 100):
+        # print("Helllo", i)
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i = i + 1
+
+fizzbuzz()
